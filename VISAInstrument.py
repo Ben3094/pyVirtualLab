fc9cb752e99bb90e0b20100d592285f04abb4785
Benjamin Saggin <benjamin.saggin@live.fr>
2022-06-24 18:24:24 +0200

    [Add] Common GPIB commands [Add] Default implementation for Abort method that falls back to reset

--- VISAInstrument.py
+++ VISAInstrument.py
@@ -1,4 +1,3 @@
-from abc import abstractmethod
 import pyvisa
 
 class VISAInstrument:
@@ -6,7 +5,20 @@ class VISAInstrument:
         self._rm = pyvisa.ResourceManager('@py')
         self._instr = self._rm.open_resource(address)
 
+    def Id(self):
+        return self._instr.query('*IDN?')
+
+    def SelfTest(self):
+        if not bool(int(self._instr.query('*TST?'))):
+            raise Exception('Error in the self test')
+    
+    def Reset(self):
+        self._instr.write('*RST')
+
 class Source(VISAInstrument):
+    def _abort(self):
+        self.Reset()
+
     def __init__(self, address):
         VISAInstrument.__init__(self, address)
         self.Abort = self._abort
