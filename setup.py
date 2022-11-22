397ccc35f724086a3b5c56d554ae4408a7307e88
Benjamin Saggin <benjamin.saggin@live.fr>
2022-11-22 16:43:49 +0100

    [Add] Package publication

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    install_requires = [line.strip() for line in fh.readlines()]
+
+setup(
+    name='pyVirtualLab',
+    version='0.0.1',
+    author='Benjamin SAGGIN',
+    description='An abstraction library for laboratory instruments',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Ben3094/pyNewportController',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Ben3094/pyNewportController/issues"
+    },
+    license='MIT',
+    py_modules=["VISAInstrument", "Instruments"],
+    install_requires=install_requires,
+)
